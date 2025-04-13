f11dd08e4b1b2945ec2d86fcad2cce0ffdd1eff1
zahidayturan <21060607@stu.omu.edu.tr>
2025-04-13 21:40:12 +0300

    prepared for superuser and admin

--- django/django-auth-api/api/settings.py
+++ django/django-auth-api/api/settings.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
