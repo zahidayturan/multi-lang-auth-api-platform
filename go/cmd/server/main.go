b55c7a275ae2454e90c3c18d9c9a914c74f6e33c
zahidayturan <21060607@stu.omu.edu.tr>
2025-09-17 20:57:13 +0300

    @go - adds user authentication service
    Implements an authentication service with JWT support.
    
    This commit introduces:
    - An AuthService with methods for password hashing, JWT generation, and parsing.
    - A test endpoint to generate tokens.
    - A test user for development purposes.

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"auth-api/internal/config"
+	"auth-api/internal/services"
 	"auth-api/internal/store"
 	"github.com/gin-gonic/gin"
 	"log"
+	"time"
 )
 
 func main() {
@@ -14,14 +16,27 @@ func main() {
 	// DB init
 	store.InitDB(cfg.DatabaseURL)
 
+	// auth service create
+	authService := &services.AuthService{
+		JWTSecret:   cfg.JWTSecret,
+		JWTExpireIn: time.Hour, // 1 hour
+	}
+
 	// router
 	r := gin.Default()
 
-	// basic health check endpoint
+	// basit health endpoint
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status": "ok",
 		})
+	})
+
+	// test token endpoint
+	r.GET("/token-test", func(c *gin.Context) {
+		dummyUser := store.DBUserForTest() // bunu az sonra göstereceğim
+		token, _ := authService.GenerateJWT(dummyUser)
+		c.JSON(200, gin.H{"token": token})
 	})
 
 	addr := ":" + cfg.Port
